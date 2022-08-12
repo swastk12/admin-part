afc83195fdaa3d88be0d48d62a42bc716988ab04
unknown <swastikdubey1234@gmail.com>
2022-08-12 15:17:29 +0530

    initial 3

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use("/images", express.static(path.join(__dirname, "/images")));
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useNewUrlParser: true,
+    useCreateIndex: true,
     useUnifiedTopology: true,
   })
   .then(console.log("Connected to MongoDB"))
