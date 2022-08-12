83dc3c52eec49544a2a92ccef6513ec21e9bfd9a
unknown <swastikdubey1234@gmail.com>
2022-08-12 13:47:24 +0530

    initial

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use("/images", express.static(path.join(__dirname, "/images")));
 
 
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
